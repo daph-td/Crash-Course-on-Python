b399d5d95f1c1585476aa6867715b6f1e36c1478
daphtd <44963656+daph-td@users.noreply.github.com>
2021-01-27 19:45:13 +0700

    Add files via upload

--- Exercises on IDE/1 Basic Syntax/ex1_print.py
+++ Exercises on IDE/1 Basic Syntax/ex1_print.py
@@ -6,11 +6,12 @@ Fill in the __ to print output the text on the screen
 print("Hello World")
 
 # Output: Hello [your name]
-print()
+name = 'Daphne'
+print('hello ', name)
 
 # Output: I love interactive videos!
-print()
+print('')
 
 # Output: How to print "Hello World"?
-print()
+print('')
 
